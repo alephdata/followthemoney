a2a944b18c23c774a36cd4eaebdb9edf8d33c44f
Friedrich Lindenberg <friedrich@pudo.org>
2017-10-20 11:01:07 +0200

    initial commit

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='followthemoney',
+    version='0.2',
+    long_description="Data model and validator for investigative entity data.",
+    keywords='',
+    author='Organized Crime and Corruption Reporting Project',
+    author_email='pudo@occrp.org',
+    url='https://occrp.org',
+    license='MIT',
+    packages=find_packages(exclude=['ez_setup', 'examples', 'test']),
+    namespace_packages=[],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'normality',
+    ],
+    test_suite='nose.collector',
+    entry_points={
+        'console_scripts': [],
+    },
+    tests_require=['coverage', 'nose']
+)
