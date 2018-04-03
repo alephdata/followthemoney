f501414b3cd72d99ee688a9118ed3c2d23d60621
jen <jen@occrp.org>
2018-04-03 16:02:29 +0200

    Bump version: 0.6.12 → 0.6.13

--- followthemoney/__init__.py
+++ followthemoney/__init__.py
@@ -3,7 +3,7 @@ import os
 from followthemoney.model import Model
 from followthemoney.util import set_model_locale
 
-__version__ = '0.6.12'
+__version__ = '0.6.13'
 
 model_path = os.path.dirname(__file__)
 model_path = os.path.join(model_path, 'schema')
