213b3af90929178b6bf51141717c1502bba685e4
rhiaro <amy@rhiaro.co.uk>
2018-05-15 10:44:32 +0200

    Bump version: 0.8.0 → 0.8.1

--- followthemoney/__init__.py
+++ followthemoney/__init__.py
@@ -3,7 +3,7 @@ import os
 from followthemoney.model import Model
 from followthemoney.util import set_model_locale
 
-__version__ = '0.8.0'
+__version__ = '0.8.1'
 
 model_path = os.path.dirname(__file__)
 model_path = os.path.join(model_path, 'schema')
