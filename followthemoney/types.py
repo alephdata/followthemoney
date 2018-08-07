e0784fa75e71c72785819085f4e3a48321b36217
Friedrich Lindenberg <friedrich@pudo.org>
2018-08-07 09:31:58 +0200

    Remove “uri” type.

--- followthemoney/types.py
+++ followthemoney/types.py
@@ -8,7 +8,6 @@ TYPES = {
     'name': PropType(exactitude.names, 'names'),
     'entity': PropType(exactitude.texts, 'entities'),
     'url': PropType(exactitude.urls, 'urls'),
-    'uri': PropType(exactitude.urls, 'urls'),
     'date': PropType(exactitude.dates, 'dates'),
     'address': PropType(exactitude.addresses, 'addresses'),
     'country': PropType(exactitude.countries, 'countries'),
