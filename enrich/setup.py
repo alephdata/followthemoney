d8731be4b672f7afe57aaf5eac318a28856b4ec9
Friedrich Lindenberg <friedrich@pudo.org>
2018-10-31 12:27:09 +0100

    Bump version: 1.3.12 → 1.3.13

--- enrich/setup.py
+++ enrich/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='followthemoney-enrich',
-    version='1.3.12',
+    version='1.3.13',
     long_description="Data enrichment toolkit",
     classifiers=[
         "Development Status :: 3 - Alpha",
--- util/setup.py
+++ util/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='followthemoney-util',
-    version='1.3.12',
+    version='1.3.13',
     long_description="FollowTheMoney command-line tool",
     classifiers=[
         "Development Status :: 3 - Alpha",
