fa69af6db7b5fb895f33a8197562067e63ecc6f6
Friedrich Lindenberg <friedrich@pudo.org>
2018-11-18 20:53:08 +0100

    Simplify override

--- followthemoney/__init__.py
+++ followthemoney/__init__.py
@@ -5,11 +5,10 @@ from followthemoney.util import set_model_locale
 
 __version__ = '1.3.15'
 
-if os.environ.get('FTM_MODEL_PATH') is None:
-    model_path = os.path.dirname(__file__)
-    model_path = os.path.join(model_path, 'schema')
-else:
-    model_path = os.path.join(os.environ.get('FTM_MODEL_PATH'), 'schema')
+
+model_path = os.path.dirname(__file__)
+model_path = os.path.join(model_path, 'schema')
+model_path = os.environ.get('FTM_MODEL_PATH', model_path)
 
 # Data model singleton
 model = Model(model_path)
