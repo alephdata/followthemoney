9399cd052096aff20167355a4058eb9d3dc4b1d0
Friedrich Lindenberg <friedrich@pudo.org>
2019-01-16 22:37:36 +0100

    Sketch implementation of Neo4J/Cypher exporter

--- util/followthemoney_util/cypher.py
+++ util/followthemoney_util/cypher.py
@@ -1,8 +1,13 @@
 import json
 import click
+import stringcase
+from banal import ensure_list
 
+from followthemoney.types import registry
 from followthemoney_util.cli import cli
 from followthemoney_util.util import read_object
+
+# MATCH (n) DETACH DELETE n;
 
 # MATCH (e:Thing {id: banana})
 # MERGE ON MATCH SET e += {name: "Fox"};
@@ -13,16 +18,89 @@ from followthemoney_util.util import read_object
 #     SET p = { name: "Hedgehog" };
 
 
-def to_map(data):
-    parts = ['{']
+def _to_map(data):
+    values = []
     for key, value in data.items():
-        parts.extend((key, ':', json.dumps(value)))
-    parts.append('}')
-    return ' '.join(parts)
+        value = '%s: %s' % (key, json.dumps(value))
+        values.append(value)
+    return ', '.join(values)
 
 
-def convert_entity(entity):
-    return 'banana'
+def _to_labels(label):
+    return ':'.join(ensure_list(label))
+
+
+def _to_attributes(proxy, edge_types):
+    attributes = {'id': proxy.id}
+    for prop, values in proxy._properties.items():
+        if prop.type not in edge_types:
+            attributes[prop.name] = prop.type.join(values)
+    return attributes
+
+
+def _make_node(attributes, label):
+    cypher = 'MERGE (p:%(label)s { %(id)s }) ' \
+             'SET p += { %(map)s } SET p:%(label)s;'
+    return cypher % {
+        'id': _to_map({'id': attributes.get('id')}),
+        'map': _to_map(attributes),
+        'label': _to_labels(label)
+    }
+
+
+def _make_edge(source, target, attributes, label):
+    cypher = 'MATCH (s { %(source)s }), (t { %(target)s }) ' \
+             'MERGE (s)-[:%(label)s { %(map)s }]->(t);'
+    label = [stringcase.constcase(l) for l in ensure_list(label)]
+    return cypher % {
+        'source': _to_map({'id': source}),
+        'target': _to_map({'id': target}),
+        'label': _to_labels(label),
+        'map': _to_map(attributes),
+    }
+
+
+def _to_node(proxy, edge_types):
+    node_id = registry.entity.rdf(proxy.id).n3()
+    attributes = _to_attributes(proxy, edge_types)
+    attributes['name'] = proxy.caption
+    attributes['id'] = node_id
+    yield _make_node(attributes, proxy.schema.names)
+
+    for prop, values in proxy._properties.items():
+        if prop.type not in edge_types:
+            continue
+        for value in ensure_list(values):
+            weight = prop.type.specificity(value)
+            if weight == 0:
+                continue
+            other_id = prop.type.rdf(value).n3()
+            if prop.range:
+                yield _make_node({'id': other_id}, prop.range.name)
+            else:
+                attributes = {'id': other_id, 'name': value}
+                yield _make_node({'id': other_id}, prop.type.name)
+            attributes = {'weight': weight}
+            yield _make_edge(node_id, other_id, attributes, prop.name)
+
+
+def _to_edge(proxy, edge_types):
+    attributes = _to_attributes(proxy, edge_types)
+    source_prop = proxy.schema.get(proxy.schema.edge_source)
+    target_prop = proxy.schema.get(proxy.schema.edge_target)
+    for (source, target) in proxy.edgepairs():
+        source = registry.entity.rdf(source).n3()
+        yield _make_node({'id': source}, source_prop.range.name)
+        target = registry.entity.rdf(target).n3()
+        yield _make_node({'id': target}, target_prop.range.name)
+        yield _make_edge(source, target, attributes, proxy.schema.name)
+
+
+def to_cypher(proxy, edge_types=(registry.entity,)):
+    if proxy.schema.edge:
+        yield from _to_edge(proxy, edge_types)
+    else:
+        yield from _to_node(proxy, edge_types)
 
 
 @cli.command('export-cypher', help="Export to Cypher script")
@@ -34,8 +112,8 @@ def export_cypher():
             entity = read_object(stdin)
             if entity is None:
                 break
-            cypher = convert_entity(entity)
-            stdout.write(cypher.decode('utf-8'))
-            stdout.write('\n')
+            for cypher in to_cypher(entity):
+                stdout.write(cypher)
+                stdout.write('\n')
     except BrokenPipeError:
         pass
