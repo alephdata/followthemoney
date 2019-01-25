8f467d1ab386cfb405ec71910ec317257dad8087
Friedrich Lindenberg <friedrich@pudo.org>
2019-01-25 14:31:43 +0100

    Bump version: 1.5.0 → 1.5.1

--- enrich/setup.py
+++ enrich/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='followthemoney-enrich',
-    version='1.5.0',
+    version='1.5.1',
     long_description="Data enrichment toolkit",
     classifiers=[
         "Development Status :: 3 - Alpha",
