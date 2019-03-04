f0bdf4ca24b39ef87bd84fef5a58a897daf69324
Friedrich Lindenberg <friedrich@pudo.org>
2019-03-04 18:04:39 +0100

    Bump version: 1.7.3 → 1.7.4

--- util/setup.py
+++ util/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='followthemoney-util',
-    version='1.7.3',
+    version='1.7.4',
     long_description="FollowTheMoney command-line tool",
     classifiers=[
         "Development Status :: 3 - Alpha",
