ac93308bd8265c01dba48061c5a8772a3b689110
Tarashish Mishra <sunu0000@gmail.com>
2019-03-06 11:47:53 +0530

    Make export functions more modular for reuse

--- util/followthemoney_util/csv.py
+++ util/followthemoney_util/csv.py
@@ -7,19 +7,23 @@ from followthemoney_util.cli import cli
 from followthemoney_util.util import read_object
 
 
+def write_headers(fh, schema):
+    fieldnames = [prop.label for prop in schema.sorted_properties]
+    writer = csv.DictWriter(fh, fieldnames=fieldnames)
+    writer.writeheader()
+
+
 def _get_csv_handler(outdir, schema, handlers):
     fh = handlers.get(schema.name)
     if fh is None:
         name = "{0}.csv".format(schema.plural)
         path = os.path.join(outdir, name)
         handlers[schema.name] = fh = open(path, 'w')
-        fieldnames = [prop.label for prop in schema.sorted_properties]
-        writer = csv.DictWriter(fh, fieldnames=fieldnames)
-        writer.writeheader()
+        write_headers(fh, schema)
     return fh
 
 
-def _write_entity(fh, entity):
+def write_entity(fh, entity):
     fieldnames = [prop.label for prop in entity.schema.sorted_properties]
     writer = csv.DictWriter(fh, fieldnames=fieldnames)
     prop_dict = {}
@@ -40,7 +44,7 @@ def export_csv(outdir):
             if entity is None:
                 break
             fh = _get_csv_handler(outdir, entity.schema, handlers)
-            _write_entity(fh, entity)
+            write_entity(fh, entity)
     except BrokenPipeError:
         raise click.Abort()
     finally:
