ae2a47d24c2e64a52559353bd41f5d71fa49e838
Friedrich Lindenberg <friedrich@pudo.org>
2019-04-06 14:11:24 +0200

    Include defaultModel in package

--- js/src/followthemoney.ts
+++ js/src/followthemoney.ts
@@ -3,3 +3,7 @@ export * from './model'
 export * from './property'
 export * from './schema'
 export * from './type'
+
+import defaultModel_ from './defaultModel.json';
+import { IModelDatum } from './model';
+export const defaultModel: IModelDatum = defaultModel_;
