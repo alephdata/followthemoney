48e177dccc92d052a18464d361b30b762725426a
Friedrich Lindenberg <friedrich@pudo.org>
2019-04-07 11:00:56 +0200

    Bump version: 1.11.7 → 1.11.8

--- integrate/setup.py
+++ integrate/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='followthemoney-integrate',
-    version='1.11.7',
+    version='1.11.8',
     long_description="FollowTheMoney record linkage UI",
     classifiers=[
         "Development Status :: 3 - Alpha",
