35f9b263b45cd386a679e11cdc17e108ac960f97
Davit Khurshudyan <davo11122@gmail.com>
2019-04-09 01:00:49 +0200

    fixed bundling issue

--- js/rollup.config.ts
+++ js/rollup.config.ts
@@ -1,10 +1,8 @@
 import resolve from 'rollup-plugin-node-resolve'
 import commonjs from 'rollup-plugin-commonjs'
 import sourceMaps from 'rollup-plugin-sourcemaps'
-import camelCase from 'lodash.camelcase'
 import typescript from 'rollup-plugin-typescript2'
 import json from 'rollup-plugin-json'
-import builtins from 'rollup-plugin-node-builtins';
 
 const pkg = require('./package.json')
 
@@ -13,7 +11,7 @@ const libraryName = 'followthemoney'
 export default {
   input: `src/index.ts`,
   output: [
-    { file: pkg.main, name: camelCase(libraryName), format: 'umd', sourcemap: true },
+    { file: pkg.main, name: libraryName, format: 'umd', sourcemap: true },
     { file: pkg.module, format: 'es', sourcemap: true },
   ],
   // Indicate here external modules you don't wanna include in your bundle (i.e.: 'lodash')
@@ -24,16 +22,14 @@ export default {
   plugins: [
     // Allow json resolution
     json(),
-    // Compile TypeScript files
-    typescript({ tsconfig: "tsconfig.json"}),
     // Allow node_modules resolution, so you can use 'external' to control
     // which external modules to include in the bundle
     // https://github.com/rollup/rollup-plugin-node-resolve#usage
-    resolve(),
+    resolve({ browser:true }),
+    // Compile TypeScript files
+    typescript({ useTsconfigDeclarationDir : true}),
     // Allow bundling cjs modules (unlike webpack, rollup doesn't understand cjs)
     commonjs(),
-    builtins({ crypto:true }),
-
     // Resolve source maps to the original source
     sourceMaps(),
   ],
