816f64bf0be4376eda23be72c12078961f24dbf9
Friedrich Lindenberg <friedrich@pudo.org>
2019-04-09 07:24:27 +0200

    Bump version: 1.11.10 → 1.11.11

--- followthemoney/__init__.py
+++ followthemoney/__init__.py
@@ -3,7 +3,7 @@ import os
 from followthemoney.model import Model
 from followthemoney.util import set_model_locale
 
-__version__ = '1.11.10'
+__version__ = '1.11.11'
 
 
 model_path = os.path.dirname(__file__)
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='followthemoney',
-    version='1.11.10',
+    version='1.11.11',
     long_description="Data model and validator for investigative graph data.",
     author='Organized Crime and Corruption Reporting Project',
     author_email='data@occrp.org',
