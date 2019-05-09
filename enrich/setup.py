ee21987b8897c6888054e9d0c43b04093685f6d7
Friedrich Lindenberg <friedrich@pudo.org>
2019-05-09 09:26:07 +0200

    Bump version: 1.11.21 → 1.11.22

--- enrich/setup.py
+++ enrich/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='followthemoney-enrich',
-    version='1.11.21',
+    version='1.11.22',
     long_description="Data enrichment toolkit",
     classifiers=[
         "Development Status :: 3 - Alpha",
--- integrate/setup.py
+++ integrate/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='followthemoney-integrate',
-    version='1.11.21',
+    version='1.11.22',
     long_description="FollowTheMoney record linkage UI",
     classifiers=[
         "Development Status :: 3 - Alpha",
