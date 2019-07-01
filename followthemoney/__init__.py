4918958dc23d49d99c6bd01930a8b9d13d1f97a6
Friedrich Lindenberg <friedrich@pudo.org>
2019-07-01 16:29:39 +0200

    Bump version: 1.14.0 → 1.14.1

--- followthemoney/__init__.py
+++ followthemoney/__init__.py
@@ -3,7 +3,7 @@ import os
 from followthemoney.model import Model
 from followthemoney.util import set_model_locale
 
-__version__ = '1.14.0'
+__version__ = '1.14.1'
 
 
 model_path = os.path.dirname(__file__)
