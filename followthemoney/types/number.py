7ec853f12fd5e7856398f1bbb97dd35c38e7a0ff
Friedrich Lindenberg <friedrich@pudo.org>
2019-08-19 17:24:26 +0200

    Re-name casting methods

--- followthemoney/types/number.py
+++ followthemoney/types/number.py
@@ -5,11 +5,15 @@ from followthemoney.util import defer as _
 
 
 class NumberType(PropertyType):
+    CAST_RE = re.compile(r'[^0-9\-\.]')
     name = 'number'
     label = _('Number')
     plural = _('Numbers')
     matchable = False
 
-    def _cast_num(self, value):
-        value = re.sub(r'[^0-9\-\.]', '', value)
-        return float(value)
+    def to_number(self, value):
+        try:
+            value = self.CAST_RE.sub('', value)
+            return float(value)
+        except Exception:
+            return
--- tests/types/test_number.py
+++ tests/types/test_number.py
@@ -8,7 +8,7 @@ numbers = registry.number
 class NumberTest(unittest.TestCase):
 
     def test_cast_num(self):
-        self.assertEqual(numbers._cast_num('1,00,000'), 100000.0)
-        self.assertEqual(numbers._cast_num(' -999.0'), -999.0)
-        self.assertEqual(numbers._cast_num('- 1,00,000.234'), -100000.234)
-        self.assertEqual(numbers._cast_num('99'), 99.0)
+        self.assertEqual(numbers.to_number('1,00,000'), 100000.0)
+        self.assertEqual(numbers.to_number(' -999.0'), -999.0)
+        self.assertEqual(numbers.to_number('- 1,00,000.234'), -100000.234)
+        self.assertEqual(numbers.to_number('99'), 99.0)
