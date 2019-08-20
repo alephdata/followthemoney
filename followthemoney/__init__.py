204d5d9d187d4271c0ea86abfe459de643c57efc
Friedrich Lindenberg <friedrich@pudo.org>
2019-08-20 17:51:29 +0200

    Bump version: 1.16.0 → 1.17.0

--- followthemoney/__init__.py
+++ followthemoney/__init__.py
@@ -3,7 +3,7 @@ import os
 from followthemoney.model import Model
 from followthemoney.util import set_model_locale
 
-__version__ = '1.16.0'
+__version__ = '1.17.0'
 
 
 model_path = os.path.dirname(__file__)
--- integrate/setup.py
+++ integrate/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='followthemoney-integrate',
-    version='1.16.0',
+    version='1.17.0',
     long_description="FollowTheMoney record linkage UI",
     classifiers=[
         "Development Status :: 3 - Alpha",
