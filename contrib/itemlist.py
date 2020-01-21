ada6d039dab7ede870dd71d31ec88eb197c799ec
Friedrich Lindenberg <friedrich@pudo.org>
2020-01-21 23:24:19 +0100

    remove cruft

--- contrib/itemlist.py
+++
@@ -1,7 +0,0 @@
-from followthemoney import model
-
-
-for schema in model:
-    if not schema.abstract:
-        label = schema.description or schema.label
-        print('%s [%s]' % (schema.name, label.strip()))
