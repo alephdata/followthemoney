81051156567ab1bacc3e0d0b303de4046df3d6e8
Friedrich Lindenberg <friedrich@pudo.org>
2020-05-12 17:14:31 +0200

    Bump version: 1.30.1 → 1.30.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md') as f:
 
 setup(
     name='followthemoney',
-    version='1.30.1',
+    version='1.30.2',
     author='Organized Crime and Corruption Reporting Project',
     author_email='data@occrp.org',
     url='https://docs.alephdata.org/developers/followthemoney',
