fe9a94b8058ac17ff0f20bc6ba4c80ad4a4122ab
Friedrich Lindenberg <friedrich@pudo.org>
2020-06-03 10:48:27 +0200

    Sign mapped data with the dataset name by default

--- followthemoney/cli/mapping.py
+++ followthemoney/cli/mapping.py
@@ -10,17 +10,17 @@ from followthemoney.cli.util import write_object, load_mapping_file
 
 @cli.command('map', help="Execute a mapping file and emit objects")
 @click.option('-o', '--outfile', type=click.File('w'), default='-')  # noqa
-@click.option('-s', '--signature', default=None, help='HMAC signature key')  # noqa
+@click.option('--sign/--no-sign', is_flag=True, default=True, help='Apply HMAC signature')  # noqa
 @click.argument('mapping_yaml', type=click.Path(exists=True))
-def run_mapping(outfile, signature, mapping_yaml):
+def run_mapping(outfile, mapping_yaml, sign=True):
     config = load_mapping_file(mapping_yaml)
     try:
-        ns = Namespace(signature)
         for dataset, meta in config.items():
+            ns = Namespace(dataset)
             for mapping in keys_values(meta, 'queries', 'query'):
                 entities = model.map_entities(mapping, key_prefix=dataset)
                 for entity in entities:
-                    if signature is not None:
+                    if sign:
                         entity = ns.apply(entity)
                     write_object(outfile, entity)
     except BrokenPipeError:
@@ -32,25 +32,25 @@ def run_mapping(outfile, signature, mapping_yaml):
 @cli.command('map-csv', help="Map CSV data from stdin and emit objects")
 @click.option('-i', '--infile', type=click.File('r'), default='-')  # noqa
 @click.option('-o', '--outfile', type=click.File('w'), default='-')  # noqa
-@click.option('-s', '--signature', default=None, help='HMAC signature key')  # noqa
+@click.option('--sign/--no-sign', is_flag=True, default=True, help='Apply HMAC signature')  # noqa
 @click.argument('mapping_yaml', type=click.Path(exists=True))
-def stream_mapping(infile, outfile, signature, mapping_yaml):
+def stream_mapping(infile, outfile, mapping_yaml, sign=True):
     sources = []
     config = load_mapping_file(mapping_yaml)
     for dataset, meta in config.items():
         for data in keys_values(meta, 'queries', 'query'):
             query = model.make_mapping(data, key_prefix=dataset)
             source = StreamSource(query, data)
-            sources.append(source)
+            sources.append((dataset, source))
 
     try:
-        ns = Namespace(signature)
         for record in StreamSource.read_csv(infile):
-            for source in sources:
+            for (dataset, source) in sources:
+                ns = Namespace(dataset)
                 if source.check_filters(record):
                     entities = source.query.map(record)
                     for entity in entities.values():
-                        if signature is not None:
+                        if sign:
                             entity = ns.apply(entity)
                         write_object(outfile, entity)
     except BrokenPipeError:
