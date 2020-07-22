5a0a39fdfad82d48b4ac5b21ae59f26786f27003
Friedrich Lindenberg <friedrich@pudo.org>
2020-07-22 19:03:54 +0200

    Bump version: 1.33.4 → 1.33.5

--- enrich/setup.py
+++ enrich/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="followthemoney-enrich",
-    version="1.33.4",
+    version="version='1.33.5'",
     long_description="Data enrichment toolkit",
     classifiers=[
         "Development Status :: 3 - Alpha",
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md") as f:
 
 setup(
     name="followthemoney",
-    version="1.33.4",
+    version="version='1.33.5'",
     author="Organized Crime and Corruption Reporting Project",
     author_email="data@occrp.org",
     url="https://docs.alephdata.org/developers/followthemoney",
