0045bc66b8cc3cddd8a251f4d9ee8fcb2296b3fb
Friedrich Lindenberg <friedrich@pudo.org>
2020-08-25 16:11:43 +0200

    Bump version: 2.1.4 → 2.1.5

--- enrich/setup.py
+++ enrich/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="followthemoney-enrich",
-    version="2.1.4",
+    version="2.1.5",
     long_description="Data enrichment toolkit",
     classifiers=[
         "Development Status :: 3 - Alpha",
--- followthemoney/__init__.py
+++ followthemoney/__init__.py
@@ -3,7 +3,7 @@ import os
 from followthemoney.model import Model
 from followthemoney.util import set_model_locale
 
-__version__ = "2.1.4"
+__version__ = "2.1.5"
 
 
 model_path = os.path.dirname(__file__)
