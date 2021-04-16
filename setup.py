77dfaec1386fb183d11aa23a45664f31417cf2fd
Friedrich Lindenberg <friedrich@pudo.org>
2021-04-16 10:14:40 +0200

    Bump version: 2.5.0 → 2.5.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md") as f:
 
 setup(
     name="followthemoney",
-    version="2.5.0",
+    version="2.5.1",
     author="Organized Crime and Corruption Reporting Project",
     author_email="data@occrp.org",
     url="https://followthemoney.readthedocs.io/",
