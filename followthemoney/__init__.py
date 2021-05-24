dc4f5f27fe04ae7f4ae1442f94fc7c087c14fdef
Steve Haffenden <steve.haffenden@gmail.com>
2021-05-24 17:44:55 +0200

    Bump version: 2.5.2 → 2.5.3

--- followthemoney/__init__.py
+++ followthemoney/__init__.py
@@ -3,7 +3,7 @@ import os
 from followthemoney.model import Model
 from followthemoney.util import set_model_locale
 
-__version__ = "2.5.2"
+__version__ = "2.5.3"
 
 
 model_path = os.path.dirname(__file__)
