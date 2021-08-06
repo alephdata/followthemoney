0239a6fea89b2942b59944379436338080e17fa8
Steve Haffenden <steve.haffenden@gmail.com>
2021-08-06 10:38:33 +0200

    I  made these changes to get the existing tests to pass (#458)
    Update rdflib and make a few small tweaks to ensure that our code is still working and tests are still passing

--- setup.py
+++ setup.py
@@ -42,8 +42,8 @@ setup(
         "python-stdnum >= 1.16, < 2.0.0",
         "pantomime >= 0.4.0, < 1.0.0",
         "pytz >= 2021.1",
-        "rdflib >=5.0.0, < 5.1.0",
-        "networkx >= 2.5, < 2.7",
+        "rdflib >= 6.0.0, < 6.1.0",
+        "networkx >=2.5, < 2.7",
         "openpyxl >= 3.0.5, < 4.0.0",
     ],
     extras_require={
