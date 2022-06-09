5fd6036f928c12138e776bcedf25900f4c904229
Steve Haffenden <steve.haffenden@occrp.org>
2022-06-09 09:12:29 +0200

    Bump version: 2.9.5 → 2.9.6

--- enrich/setup.py
+++ enrich/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="followthemoney-enrich",
-    version="2.9.5",
+    version="2.9.6",
     long_description="Data enrichment toolkit",
     classifiers=[
         "Development Status :: 3 - Alpha",
