4f84a48eda3d420dbc667318f72f31d3f163237f
Steve Haffenden <steve.haffenden@occrp.org>
2022-09-30 14:53:38 +0200

    Add bootstrap into the UI. This probably needs to be refactored a bit

--- site/pages/_app.tsx
+++ site/pages/_app.tsx
@@ -1,4 +1,6 @@
+import 'bootstrap/dist/css/bootstrap.css'
 import '../styles/globals.css'
+
 import type { AppProps } from 'next/app'
 
 function FollowTheMoneyApp({ Component, pageProps }: AppProps) {
--- site/pages/datasets.tsx
+++ site/pages/datasets.tsx
@@ -1,6 +1,8 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+
+import 'bootstrap/dist/css/bootstrap.css'
 import styles from '../styles/Home.module.css'
 
 const Datasets: NextPage = () => {
