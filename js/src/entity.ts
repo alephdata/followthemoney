196c15795971d36300e47235d09b186a50e70f12
Till Prochaska <till.prochaska@occrp.org>
2022-10-21 10:34:48 +0200

    Narrow down return types
    `getTemporalStart`/`getTemporalEnd` returning an entity wouldn’t make any sense.

--- js/src/entity.ts
+++ js/src/entity.ts
@@ -149,16 +149,16 @@ export class Entity {
    * Get a date that can be used to represent the start of the entity in a timeline.
    * If there are multiple possible dates, the earliest date is returned.
    */
-  getTemporalStart(): { property: Property, value: Value } | null {
+  getTemporalStart(): { property: Property, value: string } | null {
     const values = []
     const properties = this.schema.getTemporalStartProperties()
 
     for (const property of properties) {
-      values.push(
-        ...this.getProperty(property).map(
-          (value) => ({ property, value})
-        )
-      )
+      for (const value of this.getProperty(property)) {
+        if (typeof value === 'string') {
+          values.push({ property, value })
+        }
+      }
     }
 
     const sortedValues = values.sort(
@@ -172,16 +172,16 @@ export class Entity {
    * Get a date that can be used to represent the end of the entity in a timeline.
    * If there are multiple possible dates, the earliest date is returned.
    */
-  getTemporalEnd(): { property: Property, value: Value } | null {
+  getTemporalEnd(): { property: Property, value: string } | null {
     const values = []
     const properties = this.schema.getTemporalEndProperties()
 
     for (const property of properties) {
-      values.push(
-        ...this.getProperty(property).map(
-          (value) => ({ property, value })
-        )
-      )
+      for (const value of this.getProperty(property)) {
+        if (typeof value === 'string') {
+          values.push({ property, value })
+        }
+      }
     }
 
     const sortedValues = values.sort(
