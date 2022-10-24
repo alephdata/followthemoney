a7b065f4ce9a8b2fd88ad42c7003733e7fc4f464
Till Prochaska <till.prochaska@occrp.org>
2022-10-24 22:49:47 +0200

    Set up default code style

--- site/tailwind.config.js
+++ site/tailwind.config.js
@@ -1,4 +1,41 @@
 module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   plugins: [require('@tailwindcss/typography')],
+  theme: {
+    extend: {
+      typography: (theme) => ({
+        DEFAULT: {
+          css: {
+            code: {
+              background: theme('colors.gray.200'),
+              color: 'inherit',
+              padding: theme('space.1'),
+              fontWeight: theme('fontWeight.normal'),
+              borderRadius: theme('borderRadius.md'),
+            },
+            'code::before': {
+              content: 'none',
+            },
+            'code::after': {
+              content: 'none',
+            },
+            'pre code': {
+              background: 'transparent',
+            }
+          },
+        },
+
+        invert: {
+          css: {
+            code: {
+              background: theme('colors.gray.800'),
+            },
+            'pre code': {
+              background: 'transparent',
+            }
+          },
+        },
+      }),
+    },
+  },
 };
