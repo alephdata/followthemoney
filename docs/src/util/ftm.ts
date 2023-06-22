3ec49e0a09a2399b18217ec5d207aefa59eb3007
Till Prochaska <till.prochaska@occrp.org>
2023-06-22 12:07:05 +0200

    Display inheritance graph in model explorer (#1114)
    * Add basic inheritance graph for schema docs
    
    * Fix schema explorer TOC
    
    * Add loading indicator
    
    * Make use of Mermaid’s own theming
    
    * Refactor: Make inheritance graph reusable
    
    For example to display a full graph of the model on the explorer index page…
    
    * Implement workaround for Astro bug
    
    This is a workaround due to an Astro bug. The bug has been fixed, but we're still on an older Astro version, and using a different file extension works fine for now.
    
    * Improve dark mode
    
    * Adjust edge/arrow styles
    
    * Use schema name (instead of label) in model explorer
    
    Most people using the model explorer will probably search for the schema name as that is what they use when creating mappings, writing crawlers, debugging problems with Aleph etc. The schema label is still visible on the index page, but we now use the name consistently in headings, page titles, navigation links etc.

--- docs/src/util/ftm.ts
+++ docs/src/util/ftm.ts
@@ -1,4 +1,24 @@
 import defaultModel from '../../../js/src/defaultModel.json';
-import { Model } from '@alephdata/followthemoney';
+import { Model, Schema } from '@alephdata/followthemoney';
 
 export const model = new Model(defaultModel);
+
+type Edges = Array<[Schema, Schema]>;
+
+export function getInheritanceEdges(schemata: Array<Schema>): Edges {
+  const nodes = new Set(schemata);
+  const edges: Set<string> = new Set();
+
+  for (const node of nodes) {
+    for (const parent of node.getExtends()) {
+      if (nodes.has(parent)) {
+        edges.add([node.name, parent.name].join(':'));
+      }
+    }
+  }
+
+  return Array.from(edges).map((edge) => {
+    const [child, parent] = edge.split(':');
+    return [model.getSchema(child), model.getSchema(parent)];
+  });
+}
