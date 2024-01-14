04554ad9e63db39737668a21197e333ac20d590b
Friedrich Lindenberg <friedrich@pudo.org>
2024-01-14 13:24:55 +0100

    Bump version: 3.5.8 → 3.5.9

--- followthemoney/__init__.py
+++ followthemoney/__init__.py
@@ -3,7 +3,7 @@ import os
 from followthemoney.model import Model
 from followthemoney.util import set_model_locale
 
-__version__ = "3.5.8"
+__version__ = "3.5.9"
 
 
 model_path = os.path.dirname(__file__)
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md") as f:
 
 setup(
     name="followthemoney",
-    version="3.5.8",
+    version="3.5.9",
     author="Organized Crime and Corruption Reporting Project",
     author_email="data@occrp.org",
     url="https://followthemoney.tech/",
