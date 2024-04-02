dfa07f80a6677d27d040bac03ff58392999acdf8
Friedrich Lindenberg <friedrich@pudo.org>
2024-04-02 12:25:09 +0200

    Use orjson to dump default model in the hopes of making this more stable

--- followthemoney/cli/cli.py
+++ followthemoney/cli/cli.py
@@ -1,14 +1,14 @@
 import sys
-import json
 import click
+import orjson
 import logging
 from pathlib import Path
-from typing import Optional, TextIO
+from typing import Optional, BinaryIO, List, Any, Dict
 from banal import ensure_list
 
 from followthemoney import model
 from followthemoney.namespace import Namespace
-from followthemoney.cli.util import InPath, OutPath, path_entities, read_entities
+from followthemoney.cli.util import InPath, OutPath, path_entities
 from followthemoney.cli.util import path_writer, write_entity
 from followthemoney.proxy import EntityProxy
 
@@ -20,9 +20,10 @@ def cli() -> None:
 
 
 @cli.command("dump-model", help="Export the current schema model")
-@click.option("-o", "--outfile", type=click.File("w"), default="-")
-def dump_model(outfile: TextIO) -> None:
-    outfile.write(json.dumps(model.to_dict(), indent=2, sort_keys=True))
+@click.option("-o", "--outfile", type=click.File("wb"), default="-")
+def dump_model(outfile: BinaryIO) -> None:
+    f = orjson.OPT_INDENT_2 | orjson.OPT_SORT_KEYS
+    outfile.write(orjson.dumps(model.to_dict(), option=f))
 
 
 @cli.command("validate", help="Re-parse and validate the given data")
@@ -34,7 +35,7 @@ def validate(infile: Path, outfile: Path) -> None:
             for entity in path_entities(infile, EntityProxy, cleaned=False):
                 clean = model.make_entity(entity.schema)
                 clean.id = entity.id
-                for (prop, value) in entity.itervalues():
+                for prop, value in entity.itervalues():
                     clean.add(prop, value)
                 write_entity(outfh, clean)
     except BrokenPipeError:
@@ -46,12 +47,14 @@ def validate(infile: Path, outfile: Path) -> None:
 @click.option("-o", "--outfile", type=OutPath, default="-")  # noqa
 def import_vis(infile: Path, outfile: Path) -> None:
     with path_writer(outfile) as outfh:
-        with open(infile, "r") as infh:
-            data = json.load(infh)
+        with open(infile, "rb") as infh:
+            data: Dict[str, Any] = orjson.loads(infh.read())
             if "entities" in data:
-                entities = data.get("entities", data)
-            if "layout" in data:
+                entities: List[Dict[str, Any]] = data.get("entities", data)
+            elif "layout" in data:
                 entities = data.get("layout", {}).get("entities", data)
+            else:
+                raise click.ClickException("No entities found in VIS file")
             for entity_data in ensure_list(entities):
                 entity = EntityProxy.from_dict(model, entity_data)
                 write_entity(outfh, entity)
@@ -75,10 +78,11 @@ def sign(infile: Path, outfile: Path, signature: Optional[str]) -> None:
 @cli.command(help="Format a stream of entities to make it readable")
 @click.option("-i", "--infile", type=InPath, default="-")  # noqa
 def pretty(infile: Path) -> None:
-    stdout = click.get_text_stream("stdout")
+    stdout = click.get_binary_stream("stdout")
     try:
+        f = orjson.OPT_INDENT_2 | orjson.OPT_APPEND_NEWLINE
         for entity in path_entities(infile, EntityProxy):
-            data = json.dumps(entity.to_dict(), indent=2)
-            stdout.write(data + "\n")
+            data = orjson.dumps(entity.to_dict(), option=f)
+            stdout.write(data)
     except BrokenPipeError:
         raise click.Abort()
