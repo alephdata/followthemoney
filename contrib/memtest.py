5c27b70f34060dbd0eb1127175dbd03e948e0579
Friedrich Lindenberg <friedrich@opensanctions.org>
2024-08-08 14:12:24 +0200

    Adapt downstream

--- contrib/memtest.py
+++ contrib/memtest.py
@@ -7,7 +7,7 @@ from followthemoney.types import registry
 
 fmt = "%(name)s [%(levelname)s] %(message)s"
 logging.basicConfig(stream=sys.stderr, level=logging.INFO, format=fmt)
-registry.text.max_size = 1 * 1024 * 1024
+registry.text.total_size = 1 * 1024 * 1024
 
 dataset = balkhash.init("memtest", backend="LEVELDB")
 text = open("LICENSE", "r").read()
