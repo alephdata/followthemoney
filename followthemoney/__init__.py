98532059db8c4333f52ca7852fa7a20c23c00ed2
Friedrich Lindenberg <friedrich@opensanctions.org>
2024-11-22 17:30:19 +0100

    Bump version: 3.7.6 → 3.7.7

--- followthemoney/__init__.py
+++ followthemoney/__init__.py
@@ -3,7 +3,7 @@ import os
 from followthemoney.model import Model
 from followthemoney.util import set_model_locale
 
-__version__ = "3.7.6"
+__version__ = "3.7.7"
 
 
 model_path = os.path.dirname(__file__)
